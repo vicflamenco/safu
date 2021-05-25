a4ad7002e57d90a5059153e98b7fc0906532cadc
Victor Flamenco <vicflamenco@hotmail.com>
2021-05-25 09:27:40 +0200

    removed redis cache feature

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using safuCHARTS.Services;
-using StackExchange.Redis;
 
 namespace safuCHARTS
 {
@@ -21,10 +19,6 @@ namespace safuCHARTS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IConnectionMultiplexer>(x =>
-                ConnectionMultiplexer.Connect(Configuration.GetValue<string>("RedisConnection")));
-            services.AddSingleton<IRedisService, RedisService>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
